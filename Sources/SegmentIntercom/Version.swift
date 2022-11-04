fbe2fb3db33b481b7434b2c45cef3c171591aca6
Alan Charles <acharles@twilio.com>
2022-11-04 14:22:23 -0600

    open source release

---
+++ Sources/SegmentIntercom/Version.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alan Charles on 6/23/22.
+//
+
+
+// DO NOT MODIFY THIS FILE BY HAND!!
+// DO NOT MODIFY THIS FILE BY HAND!!
+// DO NOT MODIFY THIS FILE BY HAND!!
+// DO NOT MODIFY THIS FILE BY HAND!!
+// Use release.sh's automation.
+// BREAKING.FEATURE.FIX
+internal let __destination_version = "1.0.0"
